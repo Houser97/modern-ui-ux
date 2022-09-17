344f236fe34c9d646773d33dec798bb5a0b7ad8a
Houser97 <arturo.riverar97@gmail.com>
2022-09-17 13:44:31 -0500

    Create 'Hero' section in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import styles from './style'
 
 const App = () => (
     <div className="bg-primary overflow-hidden w-full">
+
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           NAVBAR
         </div>
       </div>
+
+      <div className={`bg-primary ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Houser
+        </div>
+      </div>
+
     </div>
 )
 
