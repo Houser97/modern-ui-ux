8a2ea8e825d731ef0de9d322cdd66fdc95ffabd8
Houser97 <arturo.riverar97@gmail.com>
2022-09-18 12:34:30 -0500

    Create Navbar section only for big screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,14 @@ const Navbar = () => {
       <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
 
       <ul className='list-none sm:flex flex-1 hidden justify-end items-center'>
-        {}
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-white text-[16px]
+          ${index !== navLinks.length - 1 ? 'mr-10' : 'mr-0'}`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
